7dbefbafa8f4bda79505e7f4998c685d5c7064d7
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-18 13:17:13 +0530

    added tailwind for better approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
   return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <div style={{ backgroundColor: "red" }}>this is child 1</div>
-      <div style={{ backgroundColor: "blue" }}>this is child 2</div>
-      <div style={{ backgroundColor: "yellow" }}>this child 3</div>
+    <div>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div style={{ backgroundColor: "red" }}>this is child 1</div>
+        <div style={{ backgroundColor: "blue" }}>this is child 2</div>
+        <div style={{ backgroundColor: "yellow" }}>this child 3</div>
+      </div>
+<br/>
+      <div className="flex">
+        <div style={{ backgroundColor: "red" }}>this is child 1</div>
+        <div style={{ backgroundColor: "blue" }}>this is child 2</div>
+        <div style={{ backgroundColor: "yellow" }}>this child 3</div>
+      </div>
     </div>
   );
 }
