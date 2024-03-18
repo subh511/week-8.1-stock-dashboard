65a01abea485fa8a640e156c3dad883604e228a0
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-18 13:24:50 +0530

    added flex with class names

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
         <div style={{ backgroundColor: "yellow" }}>this child 3</div>
       </div>
 <br/>
-      <div className="flex">
-        <div style={{ backgroundColor: "red" }}>this is child 1</div>
-        <div style={{ backgroundColor: "blue" }}>this is child 2</div>
-        <div style={{ backgroundColor: "yellow" }}>this child 3</div>
+      <div className="flex justify-around">
+        <div className="bg-red-400">this is child 1</div>
+        <div className="bg-cyan-400">this is child 2</div>
+        <div className="bg-fuchsia-400">this child 3</div>
       </div>
     </div>
   );
